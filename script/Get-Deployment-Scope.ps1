37962d6db80659e142afbccb19d60375b463a3f6
dylanbronson <dybronso@microsoft.com>
2021-02-23 13:31:27 -0800

    update deployment

--- script/Get-Deployment-Scope.ps1
+++ script/Get-Deployment-Scope.ps1
@@ -36,3 +36,42 @@ function Get-Deployment-Scope
 
     $scopeDeploymentDict
 }
+
+
+function Get-Deployment-Scope
+{
+    # Go to root directory -- can make this configurable depending on the keyword of the heirachy
+    Set-Location "$(git rev-parse --show-toplevel)\deploy"
+    $scopeDeployments = [System.Collections.ArrayList]@()
+    # Get all file location
+    $deploymentFiles = $(Get-ChildItem -Recurse -Filter "*.deployment.json")
+    foreach($directory in ($deploymentFiles.Directory | Get-Unique))
+    {
+        $priority = 1
+        $deployments = $directory | Get-ChildItem -Filter "*.deployment.json"
+        $deploymentsWithoutBase = $deployments | Where-Object {$_.name -NotMatch "base.deployment.json"}
+        foreach ($deploymentWithoutBase in $deploymentsWithoutBase)
+        {
+            $keyType = $directory.Name.Split(".")
+            $eachValue = New-Object PSObject -Property @{ Scope=""; Type=""; DeploymentFile=""; Priority="" }
+            $eachValue.Scope = $keyType[0]
+            $eachValue.Type = $keyType[1]
+            $eachValue.DeploymentFile = $deploymentWithoutBase
+            $eachValue.Priority = $priority
+            $priority = $priority + 1
+            $scopeDeployments.Add($eachValue)
+        }
+        $baseDeployment = $deployments | Where-Object {$_.name -Match "base.deployment.json"}
+        foreach ($deployment in $baseDeployment)
+        {
+            $keyType = $directory.Name.Split(".")
+            $eachValue = New-Object PSObject -Property @{ Scope=""; Type=""; DeploymentFile=""; Priority="" }
+            $eachValue.Scope = $keyType[0]
+            $eachValue.Type = $keyType[1]
+            $eachValue.DeploymentFile = $deployment
+            $eachValue.Priority = $priority
+            $priority = $priority + 1
+            $scopeDeployments.Add($eachValue)
+        }
+    }
+}
