51129ad5b3506ea85c7a804bdcd30b7a50717715
Mariana Mihova <10135329+marianan@users.noreply.github.com>
2021-02-24 07:26:20 -0800

    Logic to update device tags per applicable scope

--- Get-Scope.ps1
+++ Get-Scope.ps1
@@ -19,11 +19,38 @@ $dict = @{}
 
 foreach ($devicePath in $relativeDeviceFolders) {
     $key = (Split-Path -Leaf $devicePath).Split('.')[0]
-    $value = Get-Scope-Array -currentRelativePath $devicePath
+
+    $value = New-Object PSObject -Property @{ ScopeArray=@(); Hub="" }
+    $value.ScopeArray = Get-Scope-Array -currentRelativePath $devicePath
+    $devicePath -match "^*\w+.hub*" > $null
+    $value.Hub = $matches[0].Split('.')[0]
 
     $dict.Add($key, $value)
 }
 
 $dict
+
+#subscription id will come from github secrets
+az account set --subscription ""
+az account show
+
+#replace with cxn str, todo implement Get-HubCxnString $dict[$device].Hub using GitHub secrets
+$westhub_cxnstr = "" 
+$easthub_cxnstr = ""
+
+foreach ($device in $dict.Keys) {        
+    if( $dict[$device].Hub -eq 'east' ) {
+        $hub_cxnstr = $easthub_cxnstr
+    } else {
+        $hub_cxnstr = $westhub_cxnstr
+    }
+    
+    $formattedScopeArray = $($dict[$device].ScopeArray | ConvertTo-Json) -replace "`n"," " -replace "`r"," " -replace """","'"
+    $tagsJson = "{ 'env': $formattedScopeArray}"
+    
+    az iot hub device-twin update --device-id $device --login $hub_cxnstr --tags $tagsJson
+}
+
+
 
 
